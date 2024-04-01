7f9933311b36837d96f6e83f48eae9dab4b6f7f1
SubhamK108 <subhamk.subs@gmail.com>
2024-04-02 03:12:27 +0530

    Added the Backend Service for the Merger page.

--- Components/Merger/MergerCore.cs
+++ Components/Merger/MergerCore.cs
@@ -2,6 +2,27 @@ namespace Blazor.PDF.Toolkit.Components.Merger;
 
 public class MergerCore
 {
+    public static void MergePdfs()
+    {
+        using MemoryStream stream = new();
+        PdfWriter pdfWriter = new(stream);
+        pdfWriter.SetSmartMode(false);
+        PdfDocument pdfDocument = new(pdfWriter);
+        pdfDocument.InitializeOutlines();
+
+        foreach (ProcessedFile file in Merger.UploadedFiles)
+        {
+            PdfDocument currentPdf = new(new PdfReader(new MemoryStream(file.Content!)));
+            currentPdf.CopyPagesTo(1, currentPdf.GetNumberOfPages(), pdfDocument);
+            currentPdf.Close();
+        }
+
+        pdfDocument.Close();
+
+        Core.Core.FinalPdfFilename = "Merged PDF";
+        Core.Core.FinalPdfUrl = $"data:application/pdf;base64,{Convert.ToBase64String(stream.ToArray())}";
+    }
+
     public static void RefreshMerger()
     {
         Merger.UploadedFiles = [];
--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,3 +1,5 @@
 global using System.Text.RegularExpressions;
 
+global using iText.Kernel.Pdf;
+
 global using Blazor.PDF.Toolkit.Models;
