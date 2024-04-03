ffb08284063a4fb38bf9bd4e23f51d36cb98da6d
SubhamK108 <subhamk.subs@gmail.com>
2024-04-04 00:04:41 +0530

    Added the Backend Service for Image Converter.

--- Components/ImageConverter/ImageConverterCore.cs
+++ Components/ImageConverter/ImageConverterCore.cs
@@ -2,6 +2,70 @@ namespace Blazor.PDF.Toolkit.Components.ImageConverter;
 
 public class ImageConverterCore
 {
+    public static void ConvertImageToPdf()
+    {
+        using MemoryStream stream = new();
+        PdfDocument pdfDocument = new(new PdfWriter(stream));
+        ImageData imageData = ImageDataFactory.Create(ImageConverter.UploadedImage!.Content!);
+        Image image = new(imageData);
+
+        // Width of an A4 Page in 72 DPI = 595 Pixels
+        const int refWidth = 595;
+        // Width of an A4 Page in 96 DPI = 794 Pixels
+        const int refWidthAlt = 794;
+
+        float docWidth = 0;
+        float docHeight = 0;
+        float imageWidth = image.GetImageWidth();
+        float imageHeight = image.GetImageHeight();
+
+        float imageRatio = imageHeight / imageWidth;
+        // imageRatio > 1 => Portrait Image
+        // imageRatio < 1 => Landscape Image
+
+        if (imageRatio > 1)
+        {
+            if (imageWidth > refWidth)
+            {
+                image.SetWidth(refWidth);
+                image.SetHeight(refWidth * imageRatio);
+                docWidth = refWidth;
+                docHeight = refWidth * imageRatio;
+            }
+            else
+            {
+                docWidth = imageWidth;
+                docHeight = imageHeight;
+            }
+        }
+        else
+        {
+            if (imageWidth > refWidthAlt)
+            {
+                image.SetWidth(refWidthAlt);
+                image.SetHeight(refWidthAlt * imageRatio);
+                docWidth = refWidthAlt;
+                docHeight = refWidthAlt * imageRatio;
+            }
+            else
+            {
+                docWidth = imageWidth;
+                docHeight = imageHeight;
+            }
+        }
+
+        Document document = new(pdfDocument, new PageSize(docWidth, docHeight));
+        document.SetMargins(0, 0, 0, 0);
+        document.Add(image);
+
+        document.Close();
+        pdfDocument.Close();
+
+        string fileName = ImageConverter.UploadedImage!.FileName!;
+        Core.Core.FinalPdfFilename = $"{fileName[..(fileName.Length - 4)]} (PDF)";
+        Core.Core.FinalPdfUrl = $"data:application/pdf;base64,{Convert.ToBase64String(stream.ToArray())}";
+    }
+
     public static void RefreshImageConverter()
     {
         ImageConverter.UploadedImage = null;
--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,5 +1,9 @@
 global using System.Text.RegularExpressions;
 
 global using iText.Kernel.Pdf;
+global using iText.Kernel.Geom;
+global using iText.Layout;
+global using iText.Layout.Element;
+global using iText.IO.Image;
 
 global using Blazor.PDF.Toolkit.Models;
