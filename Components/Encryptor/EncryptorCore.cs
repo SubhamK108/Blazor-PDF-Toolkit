b5d5746e85fd92e641248865cc7dd64ba115bd92
SubhamK108 <subhamk.subs@gmail.com>
2024-04-04 01:32:44 +0530

    Added the Backend Service for Encryptor.

--- Components/Encryptor/EncryptorCore.cs
+++ Components/Encryptor/EncryptorCore.cs
@@ -2,6 +2,30 @@ namespace Blazor.PDF.Toolkit.Components.Encryptor;
 
 public class EncryptorCore
 {
+    public static void EncryptPdf()
+    {
+        using MemoryStream stream = new();
+        byte[] password = Encoding.UTF8.GetBytes(Encryptor.Password);
+        WriterProperties writerProperties = new();
+        writerProperties.SetStandardEncryption(
+            password,
+            password,
+            EncryptionConstants.ALLOW_PRINTING | EncryptionConstants.ALLOW_MODIFY_ANNOTATIONS | EncryptionConstants.ALLOW_MODIFY_CONTENTS,
+            EncryptionConstants.ENCRYPTION_AES_256 | EncryptionConstants.DO_NOT_ENCRYPT_METADATA
+        );
+        PdfWriter pdfWriter = new(stream, writerProperties);
+        PdfReader pdfReader = new(new MemoryStream(Encryptor.UploadedFile!.Content!));
+        PdfDocument pdfDocument = new(pdfReader, pdfWriter);
+
+        pdfDocument.Close();
+        pdfReader.Close();
+        pdfWriter.Close();
+
+        string fileName = Encryptor.UploadedFile!.FileName!;
+        Core.Core.FinalPdfFilename = $"{fileName[..(fileName.Length - 4)]} (Encrypted)";
+        Core.Core.FinalPdfUrl = $"data:application/pdf;base64,{Convert.ToBase64String(stream.ToArray())}";
+    }
+
     public static void RefreshEncryptor()
     {
         Encryptor.UploadedFile = null;
--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,3 +1,4 @@
+global using System.Text;
 global using System.Text.RegularExpressions;
 
 global using iText.Kernel.Pdf;
